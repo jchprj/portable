05db3844e1142e82a6d399f8582a6d67d625636e
Jue Chen <3330584+jchprj@users.noreply.github.com>
2020-07-21 23:31:10 +0800

    Ubuntu setup

--- setup_linux/ubuntu.sh
+++ setup_linux/ubuntu.sh
@@ -1,11 +1,16 @@
 sudo apt-get update -y
 sudo apt install -y htop
 sudo apt install -y net-tools
+sudo apt install -y curl
 # Check ufw status by: sudo ufw status verbose
 sudo ufw allow ssh
 sudo ufw enable
 sudo apt install -y openssh-server
 # Check Git version to be installed by: apt-cache policy git
 sudo apt install -y git-all
+sudo apt install -y vim
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 sudo apt-get install mingw-w64
