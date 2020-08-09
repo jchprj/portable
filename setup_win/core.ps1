dc028ff1bb5d05d2a0eae345244a8f3615c22b47
Jue Chen <3330584+jchprj@users.noreply.github.com>
2020-08-09 17:26:40 +0800

    WSL 2

--- setup_win/core.ps1
+++ setup_win/core.ps1
@@ -1,1 +1,4 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wsl --set-default-version 2
