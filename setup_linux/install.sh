d2f9882d56868c2a9debaf97553d89b23aef8cef
Jue Chen <3330584+jchprj@users.noreply.github.com>
2020-08-23 15:34:22 +0800

    ctop for docker

---
+++ setup_linux/install.sh
@@ -0,0 +1,3 @@
+# Find latest: https://github.com/bcicen/ctop/releases
+sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.3/ctop-0.7.3-linux-amd64 -O /usr/local/bin/ctop
+sudo chmod +x /usr/local/bin/ctop
