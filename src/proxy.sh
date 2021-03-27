cfeb9faf5735233c01698b51ba207e6f379cfd60
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-03-27 20:06:01 +0800

    Show proxy settings

---
+++ src/proxy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo 
+echo cat ~/.gitconfig
+cat ~/.gitconfig
+echo 
+echo cat ~/.npmrc
+cat ~/.npmrc
+echo 
+echo cat ~/.gradle/gradle.properties
+cat ~/.gradle/gradle.properties
