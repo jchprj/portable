23ada23b72f9792c12d6b22f0ec4ced4c4d1aaac
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-05-19 01:26:53 +0800

    ssh-copy-id.cmd

--- cmds/ssh-copy-id.cmd
+++ cmds/ssh-copy-id.cmd
@@ -1,2 +1,2 @@
 REM https://www.chrisjhart.com/Windows-10-ssh-copy-id/#:~:text=At%20the%20moment%2C%20Windows%2010's,Linux%20device%20for%20passwordless%20login.
-type %UserProfile%\.ssh\id_rsa.pub | ssh %1 "cat >> .ssh/authorized_keys"
+type %UserProfile%\.ssh\id_rsa.pub | ssh %1 %2 %3 "cat >> .ssh/authorized_keys"
