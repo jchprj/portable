07c711ce169a56c6e1d91bdb8b325ec045179337
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-07-04 16:55:11 +0800

    choco install obs-studio

--- setup_win/tools/video.cmd
+++ setup_win/tools/video.cmd
@@ -2,3 +2,6 @@ choco install -y vlc
 
 REM GPLv2. Avidemux is a free video editor designed for simple cutting, filtering and encoding tasks. It supports many file types, including AVI, DVD compatible MPEG files, MP4 and ASF, using a variety of codecs. Tasks can be automated using projects, job queue and powerful scripting capabilities.
 choco install avidemux
+
+REM GPLv2. Open Broadcaster Software, free and open source software for video recording and live streaming.
+choco install obs-studio
