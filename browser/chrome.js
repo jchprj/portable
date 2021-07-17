c2646bed8ca18291c2ddc205fa42110e40d63d89
Jue Chen <3330584+jchprj@users.noreply.github.com>
2021-07-18 00:07:02 +0800

    Chrome extensions

---
+++ browser/chrome.js
@@ -0,0 +1,102 @@
+
+var chrome = `
+
+# Chrome extensions
+
+## Core
+
+[Copy as Markdown](https://chrome.google.com/webstore/detail/fkeaekngjflipcockcnpobkpbbfbhmdn)
+
+[Dark Reader](https://chrome.google.com/webstore/detail/eimadpbcbfnmbkopoojfekhnkhdbieeh)
+
+[Export links of all extensions](https://chrome.google.com/webstore/detail/cmeckkgeamghjhkepejgjockldoblhcb)
+
+[floccus bookmarks sync](https://chrome.google.com/webstore/detail/fnaicdffflnofjppbagibeoednhnbjhg)
+
+[Google Dictionary (by Google)](https://chrome.google.com/webstore/detail/mgijmajocgfcbeboacabfgobmjgjcoja)
+
+[Google Translate](https://chrome.google.com/webstore/detail/aapbdbdomjkkjkaonfhkkikfgjllcleb)
+
+[JSONView](https://chrome.google.com/webstore/detail/chklaanhfefbnpoihckbnefhakgolnmc)
+
+[Just Black](https://chrome.google.com/webstore/detail/aghfnjkcakhmadgdomlmlhhaocbkloab)
+
+[Proxy SwitchyOmega](https://chrome.google.com/webstore/detail/padekgcemlokbadohgkifijomclgjgif)
+
+[SingleFile](https://chrome.google.com/webstore/detail/mpiodijhokgodhhofbcjdecpffjipkle)
+
+[uBlock Origin](https://chrome.google.com/webstore/detail/cjpalhdlnbpafiamejdnhcphjbkeiagm)
+
+## Dev
+
+[Chrome Regex Search](https://chrome.google.com/webstore/detail/bpelaihoicobbkgmhcbikncnpacdbknn)
+
+[GitHub Repository Size](https://chrome.google.com/webstore/detail/apnjnioapinblneaedefcnopcjepgkci)
+
+[Octotree - GitHub code tree](https://chrome.google.com/webstore/detail/bkhaagjahfmjljalopjnoealnfndnagc)
+
+[Chrome MySQL Admin](https://chrome.google.com/webstore/detail/ndgnpnpakfcdjmpgmcaknimfgcldechn)
+
+[Bang JSON workspace](https://chrome.google.com/webstore/detail/dfmpfciemnocjnpfddbefbhhamhjcmgl)
+
+[Moesif Origin & CORS Changer](https://chrome.google.com/webstore/detail/digfbfaphojjndkpccljibejjbppifbc)\
+
+## Chrome
+
+[Multi-highlight](https://chrome.google.com/webstore/detail/pfgfgjlejbbpfmcfjhdmikihihddeeji)
+
+[Slick RSS](https://chrome.google.com/webstore/detail/ealjoljnibpdkocmldliaoojpgdkcdob)
+
+[Clear Cookie and Reload](https://chrome.google.com/webstore/detail/clinijjpaohndgmfepgpifcfnmlecbom)
+
+[IE Tab](https://chrome.google.com/webstore/detail/hehijbfgiekmjfkfjpbkbammjbdenadd)
+
+[Print Friendly & PDF](https://chrome.google.com/webstore/detail/ohlencieiipommannpdfcmfdpjjmeolj)
+
+[Save as PDF](https://chrome.google.com/webstore/detail/kpdjmbiefanbdgnkcikhllpmjnnllbbc)
+
+[Web Server for Chrome](https://chrome.google.com/webstore/detail/ofhbbkphhbklhfoeikjpcbhemlocgigb)
+
+[Video Downloader by Video Octopus 🐙](https://chrome.google.com/webstore/detail/oacemiijpacmjnmhokeehdnhjbeeklod)
+
+## Google
+
+[Gmail](https://chrome.google.com/webstore/detail/pjkljhegncpnkpknbcohdijeoejaedia)
+
+[Google Keep Chrome Extension](https://chrome.google.com/webstore/detail/lpcaedmchfhocbbapmcbpinfpgnhiddi)
+
+[Google Mail Checker](https://chrome.google.com/webstore/detail/mihcahmgecmbnbcchbopgniflfhgnkff)
+
+[Google Voice (by Google)](https://chrome.google.com/webstore/detail/kcnhkahnjcbndmmehfkdnkjomaanaooo)
+
+[h264ify](https://chrome.google.com/webstore/detail/aleakchihdccplidncghkekgioiakgal)
+
+## Google Drive
+
+[Application Launcher For Drive (by Google)](https://chrome.google.com/webstore/detail/lmjegmlicamnimmfhcmpkclmigmmcbeh)
+
+[Docs](https://chrome.google.com/webstore/detail/aohghmighlieiainnegkcijnfilokake)
+
+[Google Docs Offline](https://chrome.google.com/webstore/detail/ghbmnnjooekpmoecnnnilnnbdlolhkhi)
+
+[Google Drive](https://chrome.google.com/webstore/detail/apdfllckaahabafndbhieahigkjlhalf)
+
+[Office Editing for Docs, Sheets & Slides](https://chrome.google.com/webstore/detail/gbkeegbaiigmenfmjfclcdgdpimamgkj)
+
+[Sheets](https://chrome.google.com/webstore/detail/felcaaldnbdncclmgdcncolpebgiejap)
+
+[Slides](https://chrome.google.com/webstore/detail/aapocclcgogkmnckokdopfmhonfmgoek)
+
+## Optional
+
+[Chrome Remote Desktop](https://chrome.google.com/webstore/detail/inomeogfingihgjfjlpeplalcfajhgai)
+
+[Logitech Smooth Scrolling](https://chrome.google.com/webstore/detail/dkpejdfnpdkhifgbancbammdijojoffk)
+
+## Game
+
+[Lego Builder](https://chrome.google.com/webstore/detail/mapnbjhfjionggfhlkmhjbmbpgfdlolh)
+
+## End
+
+`
