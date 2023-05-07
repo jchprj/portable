75ca927a841a008a4ecd62d41e126a5eecd1bf71
Jue Chen <3330584+jchprj@users.noreply.github.com>
2023-05-07 19:06:26 +0800

    Setup Windows

--- setup/windows10/core.ps1
+++ setup/windows10/core.ps1
@@ -1,5 +1,8 @@
 pkgmgr /iu:"TelnetClient"
 Enable-PSRemoting -SkipNetworkProfileCheck
+# https://www.sharepointdiary.com/2014/03/fix-for-powershell-script-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system.html
+Set-ExecutionPolicy RemoteSigned
+
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
